dc12b30afeea60065574a1b3744495a50f2476ee
ChrisCodever <chriscodever@gmail.com>
2023-10-26 16:38:07 -0300

    Segundo modal

---
+++ inserir.js
@@ -0,0 +1,17 @@
+function openModal(chave) {
+  let modal = document.getElementById(chave);
+
+  if (typeof modal == 'undefined' || modal === null)
+    return;
+  modal.style.display = 'block';
+
+}
+
+function closeModal(chave) {
+  let modal = document.getElementById(chave);
+
+  if (typeof modal == 'undefined' || modal === null)
+    return;
+  modal.style.display = 'none';
+
+}
